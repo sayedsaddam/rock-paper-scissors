fb7e63cd00794969ab42a018ff92c223da244f5b
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-03-22 11:12:46 +0500

    Learning JS
    ... created a game called rock-paper-scissors using Javascript

---
+++ app.js
@@ -0,0 +1,54 @@
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  userChoice = e.target.id;
+  userChoiceDisplay.innerHTML = userChoice;
+  generateComputerChoice();
+  getResult();
+}));
+
+function generateComputerChoice(){
+   const randomNumber = Math.floor(Math.random() * 3) + 1;// we can write possibleChoices.length too.
+   // console.log(randomNumber);
+   if(randomNumber === 1){
+      computerChoice = 'rock';
+   }
+   if(randomNumber === 2){
+      computerChoice = 'paper';
+   }
+   if(randomNumber === 3){
+      computerChoice = 'scissors';
+   }
+   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult(){
+   if(computerChoice === userChoice){
+      result = "It's a tie!";
+   }
+   if(computerChoice === 'rock' && userChoice === 'paper'){
+      result = "You win!";
+   }
+   if(computerChoice === 'rock' && userChoice === 'scissors'){
+      result = "You lose!";
+   }
+   if(computerChoice === 'paper' && userChoice === 'scissors'){
+      result = "You win!";
+   }
+   if(computerChoice === 'paper' && userChoice === 'rock'){
+      result = "You lose!";
+   }
+   if(computerChoice === 'scissors' && userChoice === 'rock'){
+      result = "You win!";
+   }
+   if(computerChoice === 'sciosors' && userChoice === 'paper'){
+      result = "You lose!";
+   }
+   resultDisplay.innerHTML = result;
+}
